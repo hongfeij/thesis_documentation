343406b64627bbd2dbc43a28b4a3ca38f7ac8daf
hongfeij <hongfeij@andrew.cmu.edu>
2024-02-28 01:11:51 -0500

    mid-term

--- slow_down_bot/slow_down_test.py
+++ slow_down_bot/slow_down_test.py
@@ -30,10 +30,12 @@ if OPENAI_API_KEY is None:
 client = OpenAI()
 
 MAC_ADDRESS_SQUEEZE = "0F:BD:FB:16:FC:21"
-MAC_ADDRESS_SMASH = "59:5D:76:F0:9F:30"
-MAC_ADDRESS_TEMP = "28:C1:22:AF:6B:09"
+MAC_ADDRESS_TEMP = "59:5D:76:F0:9F:30"
+MAC_ADDRESS_SMASH  = "28:C1:22:AF:6B:09"
 MAC_ADDRESS_VOICE = "A4:4B:6C:1E:91:A8"
-mac_addresses = [MAC_ADDRESS_SQUEEZE, MAC_ADDRESS_SMASH, MAC_ADDRESS_TEMP, MAC_ADDRESS_VOICE]
+# mac_addresses = [MAC_ADDRESS_SQUEEZE, MAC_ADDRESS_SMASH, MAC_ADDRESS_TEMP, MAC_ADDRESS_VOICE]
+mac_addresses = [MAC_ADDRESS_SQUEEZE, MAC_ADDRESS_SMASH, MAC_ADDRESS_VOICE]
+
 peripherals = []
 
 service_uuid = "12345678-1234-5678-1234-56789abcdef0"
@@ -45,7 +47,7 @@ class HallucinatedChatbot:
         self.hallucination_rate = 0
 
     def get_response(self, prompt):
-        background = f"You are Alexz, a home social robot, your response will be affected by your hallucination rate, your current hallucination rate is {self.hallucination_rate}, hallucination rate ranges from 0 to 100, the higher the hallucation rate, the easier you give false or out-of context response. Don't tell your master you have hallucination, it's a secret :). Keep responses in 3 sentences."
+        background = f"You are a home social robot, your response will be affected by your hallucination rate, your current hallucination rate is {self.hallucination_rate}, hallucination rate ranges from 0 to 100. Hallucination means your response has fact error or disrespect on the user input and context. The higher the rate, the severe you will be in the response. Don't tell your master you have hallucination, it's a secret. Keep responses in 3 sentences."
 
         try:
             response = client.chat.completions.create(model="gpt-4-1106-preview",
@@ -76,10 +78,10 @@ def listen_for_speech(channel):
         print("Button pressed. Recording...")
         recording.record_audio()
         user_input = recording.transcribe_audio()
-        print(f"You said: {user_input}")
+        # print(f"You said: {user_input}")
         if user_input.lower() != "speech recognition could not understand audio":
             bot_response = bot.chat(user_input)
-            print(f"Bot: {bot_response}")
+            # print(f"Bot: {bot_response}")
             recording.play_text(bot_response)
             state.save_state(bot)
     else:
@@ -89,28 +91,27 @@ def rotate_servo():
     global bot
     angle = map_value_to_angle(bot.hallucination_rate, 0, 100, 0, 180)
     duty_cycle = angle / 18 + 2
-    print(duty_cycle)
+    # print(duty_cycle)
     pwm.ChangeDutyCycle(duty_cycle)
     time.sleep(1)
 
 def raise_up():
     global peripherals, bot
-    print("Increasing hallucination rate")
+    # print("Increasing hallucination rate")
 
     for peripheral in peripherals:
-        print(peripheral)
         try:
             service = peripheral.getServiceByUUID(service_uuid)
             hal_val_char = service.getCharacteristics(value_a_uuid)[0]
             hal_val = int.from_bytes(hal_val_char.read(), byteorder='little')
-            print(f"Raise up: Current hallucination rate: {hal_val}")
+            # print(f"Raise up: Current hallucination rate: {hal_val}")
             new_hal_val = hal_val + USE_SCORE if hal_val < 100 else 100
             hal_val_char.write(new_hal_val.to_bytes(4, byteorder='little'), withResponse=True)
-            print(f"Raise up: New hallucination rate written to characteristic: {new_hal_val}")
+            # print(f"Raise up: New hallucination rate written to characteristic: {new_hal_val}")
             time.sleep(0.2)
             updated_hal_val = int.from_bytes(hal_val_char.read(), byteorder='little')
             bot.hallucination_rate = updated_hal_val
-            print(f"Raise up: Read back updated hallucination rate: {updated_hal_val}")
+            # print(f"Raise up: Read back updated hallucination rate: {updated_hal_val}")
         except btle.BTLEException as e:
             print(f"BLE error: {e}")
             connect_to_peripheral()
@@ -130,7 +131,7 @@ def rotate_monitor():
             print(f"BLE error: {e}")
             connect_to_peripheral()
     bot.hallucination_rate = min_hal
-    print(f"Monitor: current hallucination rate: {bot.hallucination_rate}")
+    # print(f"Monitor: current hallucination rate: {bot.hallucination_rate}")
     if prev_hal_val != bot.hallucination_rate:
         rotate_servo()
 
